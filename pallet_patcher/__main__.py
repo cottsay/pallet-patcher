2b0edd134ad868c15142d7877db0161b97e32380
Scott K Logan <logans@cottsay.net>
2025-02-12 16:55:41 -0600

    Initial implementation

--- pallet_patcher/__main__.py
+++ pallet_patcher/__main__.py
@@ -1,4 +1,10 @@
 # Copyright 2025 Open Source Robotics Foundation, Inc.
 # Licensed under the Apache License, Version 2.0
 
-__version__ = '0.0.1'
+import sys
+
+from pallet_patcher.command import main
+
+
+if __name__ == '__main__':
+    sys.exit(main())
